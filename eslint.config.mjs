b46618206a1376db4d89beace68d887bd0af164c
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2025-10-08 22:40:55 +0700

    feat: Setup Project Next JS 15 & Shadcn UI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+
+eslintConfig.push({
+	rules: {
+		"react-hooks/exhaustive-deps": "off", // untuk mengatasi warning useEffect
+		"react/jsx-key": "off", // untuk mengatasi warning key pada map
+		"@typescript-eslint/no-explicit-any": "off", // untuk mengatasi warning any pada typescript
+		"@typescript-eslint/non-unused-vars": "off", // untuk mengatasi warning unused vars pada typescript
+	},
+});
 
 export default eslintConfig;
